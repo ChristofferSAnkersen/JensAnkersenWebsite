13673d18e01a9fa59721825cae46ae1028590083
Christoffer Sohn Ankersen <chri5122@edu.eal.dk>
2021-02-01 00:47:18 +0100

    Added subsection to 'Om mig' - dropdown

---
+++ JensAnkersen/Controllers/TheoryController.cs
@@ -0,0 +1,28 @@
+﻿using JensAnkersen.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+
+namespace JensAnkersen.Controllers
+{
+    public class TheoryController : Controller
+    {
+        [ResponseCache(Duration = 600000)]
+        public IActionResult SystemiskOgNarrativ()
+        {
+            // Use this to set the name of the controller to use to-top functions
+            string controllerName = this.ControllerContext.RouteData.Values["controller"].ToString();
+            if (!String.IsNullOrWhiteSpace(controllerName))
+            {
+                return View(new ControllerName() { ControllerString = controllerName });
+            }
+            return View(new ControllerName());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
